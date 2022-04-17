1cd566b41193874e098c149184f6a8f760748830
belbeeno <belbeeno@gmail.com>
2022-04-17 13:35:21 -0700

    making shorthand notes expand to full entries (for indexing) padding short entries with underscores to work around innoDB token size (don't have access to settings) adding dry run adding verbose printing for the filters

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::fs::{File};
 use std::io::Read;
 use std::io::Write;
 use std::path::Path;
-use std::time::Instant;
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
 
 mod vox_utils;
 pub use crate::vox_utils::filters;
@@ -45,12 +45,11 @@ struct VoxIndexData {
     indexed_content: String,
     has_song: bool,
     has_morshu: bool,
+    has_grant: bool,
 }
-/*
 impl VoxIndexData {
-    fn to_string(&self) -> String { format!("ID:[{}] SONG:[{}] MORSHU:[{}] CONTENT:[{}]", self.id, self.has_song, self.has_morshu, self.indexed_content) }
+    fn to_string(&self) -> String { format!("ID:[{}] SONG:[{}] MORSHU:[{}] GRANT: [{}] \nCONTENT:[{}]\n", self.id, self.has_song, self.has_morshu, self.has_grant, self.indexed_content) }
 }
-*/
 
 // MArio is missing
 // SELECT * FROM `voxes` WHERE `id` = (SELECT `id` FROM `vox_meta` WHERE MATCH(`indexed_content`) AGAINST("mario"));
@@ -62,6 +61,7 @@ fn main() -> io::Result<()> {
         println!(" n - pull new voxes into the DB, and index them");
         println!(" m - pull voxes from file into the DB and index them");
         println!(" f YYYY-MM-DD-voxlog.txt - force pull existing log and index it");
+        println!(" d [YYYY-MM-DD-voxlog.txt] - dry run (with optional speicific file)");
         println!(" q - quit");
     }
 
@@ -75,7 +75,8 @@ fn main() -> io::Result<()> {
             None => "",
             Some(_cmd) => _cmd,
         };
-        if command == "n" {
+        let letter = command.to_string().as_bytes()[0] as char;
+        if letter == 'n' {
             // pull new voxes
             println!("Retreiving vox listing...");
             let total_now = Instant::now();
@@ -92,11 +93,11 @@ fn main() -> io::Result<()> {
                 else {
                     println!("Retreiving entry [{}]...", listing.id);
                     let now = Instant::now();
-                    collect_and_commit(&listing, &mut conn);
+                    collect_and_commit(&listing, &mut conn, false);
                     println!("Entry retrieved in [{}ms], indexing...", now.elapsed().as_millis());
                     let mut errs : Vec<(u64, String)> = Vec::new();
                     let now = Instant::now();
-                    index_log(&listing.id, &mut conn, &mut errs);
+                    index_log(&listing.id, &mut conn, &mut errs, false);
                     println!("Indexing for entry [{}] complete in [{}ms]", listing.id, now.elapsed().as_millis());
                     print_report_to_file(listing.id, errs);
                 }
@@ -104,7 +105,7 @@ fn main() -> io::Result<()> {
 
             println!("Pull complete!  Total time: [{}s]", total_now.elapsed().as_secs());
         }
-        else if command == "f" {
+        else if letter == 'f' {
             // force pull existing log
             let opts = Opts::from_url(&get_db_path()).unwrap();
             let pool = Pool::new(opts).unwrap();
@@ -116,13 +117,13 @@ fn main() -> io::Result<()> {
                 println!("Force syncing entry for {log_id}");
                 let now = Instant::now();
                 let mut errs : Vec<(u64, String)> = Vec::new();
-                index_log(&log_id, &mut conn, &mut errs);
+                index_log(&log_id, &mut conn, &mut errs, false);
                 print_report_to_file(log_id.to_string(), errs);
                 println!("Force update complete in [{}s]!", now.elapsed().as_millis());
                 iter = params_iter.next();
             }
         }
-        else if command == "m" {
+        else if letter == 'm' {
             // force pull existing log
             let opts = Opts::from_url(&get_db_path()).unwrap();
             let pool = Pool::new(opts).unwrap();
@@ -141,12 +142,12 @@ fn main() -> io::Result<()> {
                     id: path.file_name().unwrap().to_str().unwrap().to_string(),
                     date: parsed_date.format("%Y-%m-%d").to_string(),
                 };
-                load_and_commit(&listing, path, &mut conn);
+                load_and_commit(&listing, path, &mut conn, false);
                 println!("Entry retrieved in [{}ms], indexing...", now.elapsed().as_millis());
                 let mut errs : Vec<(u64, String)> = Vec::new();
                 let now = Instant::now();
                 match path.file_name() {
-                    Some(s) => index_log(s.to_str().unwrap(), &mut conn, &mut errs),
+                    Some(s) => index_log(s.to_str().unwrap(), &mut conn, &mut errs, false),
                     None => eprintln!("Path submitted for m has no filename"),
                 }
                 print_report_to_file(listing.id, errs);
@@ -154,7 +155,47 @@ fn main() -> io::Result<()> {
                 iter = params_iter.next();
             }
         }
-        else if command != "q" {
+        else if letter == 'd' {
+            println!("Performing dry run...");
+            clear_dry_run_log();
+            // force pull existing log
+            let opts = Opts::from_url(&get_db_path()).unwrap();
+            let pool = Pool::new(opts).unwrap();
+            let mut conn = pool.get_conn().unwrap();
+            let now = Instant::now();
+
+            let mut listings : Vec<Listing> = Vec::new();
+            let mut iter = params_iter.next();
+            if iter != None {
+                while iter != None {
+                    let param_listing = iter.unwrap();
+                    println!("Adding entry [{param_listing}]");
+                    let parsed_data : Date<Utc> = parse_date_from_filename(param_listing.to_string());
+                    let listing = Listing {
+                        id: param_listing.to_string(),
+                        date: parsed_data.format("%Y-%m-%d").to_string(),
+                    };
+                    listings.push(listing);
+                    iter = params_iter.next();
+                }
+            }
+            else {
+                listings = get_vox_listing();
+            }
+
+            for listing in listings {
+                let listingnow = Instant::now();
+                println_dry_run_log(format!("Processing listing: {}", listing.to_string()), true);
+                collect_and_commit(&listing, &mut conn, true);
+                println_dry_run_log(format!("Entry retrieved in [{}ms], indexing...", listingnow.elapsed().as_millis()), true);
+                let mut errs : Vec<(u64, String)> = Vec::new();
+                let listingnow = Instant::now();
+                index_log(&listing.id, &mut conn, &mut errs, true);
+                println_dry_run_log(format!("Indexing complete [{}ms].", listingnow.elapsed().as_millis()), true);
+            }
+            println!("Dry run complete in [{}s]!", now.elapsed().as_millis());
+        }
+        else if letter != 'q' {
             println!("\nUnhandled command \"{input}\"");
         }
     }
@@ -191,7 +232,8 @@ fn get_vox_listing() -> Vec<Listing> {
     listings
 }
 
-fn index_log(log_id:&str, conn:&mut PooledConn, errs:&mut Vec<(u64, String)>) {
+// This builds the indexed data off of the main data from the DB.
+fn index_log(log_id:&str, conn:&mut PooledConn, errs:&mut Vec<(u64, String)>, dryrun:bool) {
     let query = format!("SELECT `id`, `content` FROM `voxes` WHERE `log_id` = \"{log_id}\"");
     let voxes = conn.query_map(query,
         |(new_id, new_content)| {
@@ -203,6 +245,9 @@ fn index_log(log_id:&str, conn:&mut PooledConn, errs:&mut Vec<(u64, String)>) {
                 content:new_content,
             }
         },).unwrap();
+    if voxes.len() == 0 && dryrun {
+        println_dry_run_log(format!("No entry in DB found for {log_id}, can not index"), true);
+    }
 
     let mut vox_index_data : Vec<VoxIndexData> = Vec::new();
     // Get the db access sorted before we start looping
@@ -210,18 +255,23 @@ fn index_log(log_id:&str, conn:&mut PooledConn, errs:&mut Vec<(u64, String)>) {
     let pool = Pool::new(opts).unwrap();
     let mut conn = pool.get_conn().unwrap();
     for vox in voxes {
-        //println!("-- VoxEntry -- {}", vox.to_string());
+        //println!("-- VoxEntry -- {}", vox.content.to_string());
         let has_song :bool = vox.content.contains("^s");
-        let has_morshu :bool = vox.content.contains("^m");
+        let has_morshu :bool = vox.content.contains("^m") | vox.content.contains("^morshu");
+        let has_grant :bool = vox.content.contains("^g") | vox.content.contains("^grant") | vox.content.contains("^dk");
+        // ^v ix ignored
 
         // Perform filtering
         let cleaned_vox = filters::cleanup(
+                            filters::pad_short_words(
+                            filters::remap_note_shorthand(
                             filters::contractions(
                             filters::control_codes(
                             filters::pitch(
                             filters::pause(
                             filters::trunc( 
-                            filters::sanatize( vox.content.to_lowercase() )))))));
+                            filters::commands(
+                            filters::sanatize( vox.content.to_lowercase() ))))))))));
         let content_arr : Vec<&str> = cleaned_vox.split(' ').collect();
         let mut indexed_content = String::new();
         let mut used_words = HashSet::new();
@@ -235,7 +285,12 @@ fn index_log(log_id:&str, conn:&mut PooledConn, errs:&mut Vec<(u64, String)>) {
                 }
                 else {
                     errs.push((vox.id, trimmed.to_string()));
-                    println!("-- Vox entry [{}] has word [{}] that is not in the vocab.  Dropping...", vox.id, trimmed);
+                    if dryrun {
+                        println_dry_run_log(format!("-- Vox entry [{}] has word [{}] that is not in the vocab.  Dropping...", vox.id, trimmed), true);
+                    }
+                    else {
+                        println!("-- Vox entry [{}] has word [{}] that is not in the vocab.  Dropping...", vox.id, trimmed);
+                    }
                 }
             }
         }
@@ -245,20 +300,28 @@ fn index_log(log_id:&str, conn:&mut PooledConn, errs:&mut Vec<(u64, String)>) {
             indexed_content,
             has_song,
             has_morshu,
+            has_grant,
         });
     }
 
     println!("Index data for [{log_id}] compiled, sending to server...");
-    conn.exec_batch(
-    r"REPLACE INTO vox_meta (id, indexed_content, has_song, has_morshu)
-    VALUES (:author, :log_id, :date, :content)",
-    vox_index_data.iter().map(|p| params!{
-        "author" => p.id,
-        "log_id" => p.indexed_content.clone(),
-        "date" => p.has_song,
-        "content" => p.has_morshu,
-     })).unwrap();
-
+    if dryrun {
+        for vox_index_entry in vox_index_data {
+            println_dry_run_log(vox_index_entry.to_string(), false);
+        }
+    }
+    else {
+        conn.exec_batch(
+        r"REPLACE INTO vox_meta (id, indexed_content, has_song, has_morshu, has_grant)
+        VALUES (:author, :indexed_content, :has_song, :has_morshu, :has_grant)",
+        vox_index_data.iter().map(|p| params!{
+            "author" => p.id,
+            "indexed_content" => p.indexed_content.clone(),
+            "has_song" => p.has_song,
+            "has_morshu" => p.has_morshu,
+            "has_grant" => p.has_grant,
+         })).unwrap();
+    }
 }
 
 fn commit(listing:&Listing, body:String, conn:&mut PooledConn) {
@@ -287,17 +350,21 @@ fn commit(listing:&Listing, body:String, conn:&mut PooledConn) {
          })).unwrap();
 }
 
-fn collect_and_commit(listing:&Listing, conn:&mut PooledConn) {
+fn collect_and_commit(listing:&Listing, conn:&mut PooledConn, dryrun:bool) {
     // Get the voxes for each listing (as identified inside the hrefs above)
     let listing_path = format!("https://rook.zone/voxlogs/{}", listing.id);
     let listing_req = reqwest::blocking::get(listing_path).unwrap();
     let listing_body = listing_req.text().unwrap();
-    println!("{}", listing.to_string());
 
-    commit(listing, listing_body, conn);
+    if dryrun {
+        println_dry_run_log(format!("============={}=============\n{}\n=======================================", listing.to_string(), listing_body), true);
+    }
+    else {
+        commit(listing, listing_body, conn);
+    }
 }
 
-fn load_and_commit(listing:&Listing, path:&Path, conn:&mut PooledConn) {
+fn load_and_commit(listing:&Listing, path:&Path, conn:&mut PooledConn, dryrun:bool) {
     let path_str = match path.to_str() {
         Some(str) => str,
         None => "Undefined",
@@ -311,8 +378,12 @@ fn load_and_commit(listing:&Listing, path:&Path, conn:&mut PooledConn) {
     let mut file_body = String::new();
     match file.read_to_string(&mut file_body) {
         Ok(_size) => {
-            println!("{}", listing.to_string());
-            commit(listing, file_body, conn)
+            if dryrun {
+                println_dry_run_log(format!("============={}=============\n{}\n=======================================", listing.to_string(), file_body), true);
+            }
+            else {
+                commit(listing, file_body, conn)
+            }
         },
         Err(e) => eprintln!("Couldn't load voxes from file [{path_str}] because [{}]", e.to_string()),
     }
@@ -337,7 +408,7 @@ fn print_report_to_file(log_id:String, errors:Vec<(u64,String)>) {
 
     let mut file : std::fs::File = match File::options().append(true).create(true).open(&path) {
         Ok(ret) => ret,
-        Err(e) => panic!("Could not create report [{}], reason: [{}]", display, e),
+        Err(e) => panic!("Could not create report [{display}], reason: [{e}]"),
     };
 
     if let Err(e) = writeln!(file, "=== Report for [{}] - Error Count: {} ===", log_id, errors.len()) {
@@ -354,6 +425,36 @@ fn print_report_to_file(log_id:String, errors:Vec<(u64,String)>) {
         if let Err(e) = writeln!(file, "[{}] - {}", id, content) {
             eprintln!("Couldn't print to file [{display}], reason[{e}]");
             return;
+        }
+    }
+}
+
+const DRYRUN_PATH:&str = "dry_run.txt";
+static mut DRYRUN_TIME : SystemTime = UNIX_EPOCH;
+
+fn clear_dry_run_log() {
+    let result = File::options().write(true).truncate(true).create(true).open(&DRYRUN_PATH);
+    result.unwrap();
+    unsafe {
+        DRYRUN_TIME = SystemTime::now();
+    }
+}
+
+fn println_dry_run_log(val:String, also_print:bool) {
+    if also_print {
+        println!("{val}");
+    }
+
+    let mut file : std::fs::File = match File::options().append(true).open(&DRYRUN_PATH) {
+        Ok(ret) => ret,
+        Err(e) => panic!("Could not create report [{DRYRUN_PATH}], reason[{e}]"),
+    };
+
+    let start = SystemTime::now();
+    unsafe{
+        let dur = start.duration_since(DRYRUN_TIME).expect("Time went backwards");
+        if let Err(e) = writeln!(file, "[{}] {}", dur.as_millis(), val) {
+            eprintln!("Couldn't print to file [{DRYRUN_PATH}], reason[{e}]");
         }
     }
 }
--- src/vox_utils.rs
+++ src/vox_utils.rs
@@ -5,44 +5,165 @@ use std::fs::File;
 use std::io::{self, BufRead};
 
 // Filters for strings sent to `vox_meta`
+lazy_static! { static ref COMMAND_RX: Regex = Regex::new(r"^!(tc|op) vox ").unwrap(); }
 lazy_static! { static ref TRUNC_RX: Regex = Regex::new(r"[><]\.[0-9]+").unwrap(); }
 lazy_static! { static ref PAUSE_RX: Regex = Regex::new(r"[,.?!]").unwrap(); }
 lazy_static! { static ref PITCH_RX: Regex = Regex::new(r"([0-9+-]{0,20})([a-zA-Z_*']+[a-zA-Z0-9_]*)([0-9+-]{0,20})").unwrap(); }
 lazy_static! { static ref CONTROL_CODES_RX: Regex = Regex::new(r"(\^[a-zA-Z0-9=]*)|(\*)").unwrap(); }
 lazy_static! { static ref CONTRACTION_RX : Regex = Regex::new(r"(('s)|(n't))").unwrap(); }
+lazy_static! { static ref SHORTHAND_DICTIONARY : Vec<(Regex, &'static str)> = vec![
+	(Regex::new(r"\bn1\b").unwrap(), "cnote"),
+	(Regex::new(r"\bn2\b").unwrap(), "catnote"),
+	(Regex::new(r"\bn3\b").unwrap(), "cuicanote"),
+	(Regex::new(r"\bn4\b").unwrap(), "dootnote"),
+	(Regex::new(r"\bn5\b").unwrap(), "yossynote"),
+	(Regex::new(r"\bn6\b").unwrap(), "puhnote"),
+	(Regex::new(r"\bn7\b").unwrap(), "bupnote"),
+	(Regex::new(r"\bn8\b").unwrap(), "dantnote"),
+	(Regex::new(r"\bn9\b").unwrap(), "downote"),
+	(Regex::new(r"\bn10\b").unwrap(), "slapnote"),
+	(Regex::new(r"\bn11\b").unwrap(), "jarnote"),
+	(Regex::new(r"\bn12\b").unwrap(), "orchnote"),
+	(Regex::new(r"\bn13\b").unwrap(), "shynote"),
+	(Regex::new(r"\bn14\b").unwrap(), "morshunote"),
+	(Regex::new(r"\bn15\b").unwrap(), "hazymazenote"),
+	(Regex::new(r"\bn16\b").unwrap(), "hauntnote"),
+	(Regex::new(r"\bn17\b").unwrap(), "pizzicatonote"),
+	(Regex::new(r"\bn18\b").unwrap(), "zunnote"),
+	(Regex::new(r"\bn19\b").unwrap(), "banjonote"),
+	(Regex::new(r"\bn20\b").unwrap(), "banjonote2"),
+	(Regex::new(r"\bn21\b").unwrap(), "banjonote3"),
+	(Regex::new(r"\bn22\b").unwrap(), "diddynote"),
+	(Regex::new(r"\bn23\b").unwrap(), "diddynote2"),
+	(Regex::new(r"\bn24\b").unwrap(), "diddynote3"),
+	(Regex::new(r"\bkk1\b").unwrap(), "kk_na"),
+	(Regex::new(r"\bkk2\b").unwrap(), "kk_mi"),
+	(Regex::new(r"\bkk3\b").unwrap(), "kk_me"),
+	(Regex::new(r"\bkk4\b").unwrap(), "kk_o"),
+	(Regex::new(r"\bkk5\b").unwrap(), "kk_oh"),
+	(Regex::new(r"\bkk6\b").unwrap(), "kk_way"),
+	(Regex::new(r"\bkk7\b").unwrap(), "kk_now"),
+	(Regex::new(r"\bkk8\b").unwrap(), "kk_whistle"),
+	(Regex::new(r"\bkk9\b").unwrap(), "kk_howl"),
+	(Regex::new(r"\bkk10\b").unwrap(), "kk_hm"),
+	(Regex::new(r"\bkk11\b").unwrap(), "kk_hmlow"),
+	(Regex::new(r"\bkk12\b").unwrap(), "kk_snare"),
+	(Regex::new(r"\bkk13\b").unwrap(), "kk_snare2"),
+	(Regex::new(r"\bkk14\b").unwrap(), "kk_hat"),
+	(Regex::new(r"\bd1\b").unwrap(), "sonic_snare"),
+	(Regex::new(r"\bd2\b").unwrap(), "sonic_kick"),
+	(Regex::new(r"\bd3\b").unwrap(), "sonic_go"),
+	(Regex::new(r"\bd4\b").unwrap(), "hazymazedrum"),
+	(Regex::new(r"\bd5\b").unwrap(), "hazymazewood"),
+	(Regex::new(r"\bd6\b").unwrap(), "yosbongonote"),
+	(Regex::new(r"\brn\b").unwrap(), "restnote"),
+	]; }
+lazy_static! { static ref TOO_SHORT_RX : Regex = Regex::new(r"(^| )([a-zA-Z0-9_']{1,2})($|[\r\n\s ])").unwrap(); }
 lazy_static! { static ref CLEANUP_RX : Regex = Regex::new(r"( [ ]+)").unwrap(); }
 
+const VERBOSE : bool = false;
+
 pub mod filters {
+	use crate::vox_utils::VERBOSE;
+
+	fn print_if_verbose(step_name:&str, vox:&String) {
+		if VERBOSE {
+			println!("Step \"{step_name}\": [{vox}]");			
+		}
+
+	}
+
 	/////////////////////////////////////////////
 	// Filters for strings sent to `voxes`
 	pub fn sanatize(vox:String) -> String {
-		vox.replace("\"", "").replace("‘", "'").replace("’", "'")
+		let output = vox.replace("\"", "").replace("‘", "'").replace("’", "'");
+		print_if_verbose("sanatize", &output);
+		output
 	}
 
 	/////////////////////////////////////////////
 	// Filters for strings sent to `vox_meta`
+	use crate::vox_utils::COMMAND_RX;
+	pub fn commands(vox:String) -> String 
+	{
+		let output = COMMAND_RX.replace_all(&vox, "").to_string();
+		if VERBOSE { print_if_verbose("commands", &output); }
+		output
+	}
+
 	use crate::vox_utils::TRUNC_RX;
-	pub fn trunc(vox:String) -> String { TRUNC_RX.replace_all(&vox, "").to_string() }
+	pub fn trunc(vox:String) -> String 
+	{
+		let output = TRUNC_RX.replace_all(&vox, "").to_string();
+		if VERBOSE { print_if_verbose("trunc", &output); }
+		output
+	}
 
 	use crate::vox_utils::PAUSE_RX;
-	pub fn pause(vox:String) -> String { PAUSE_RX.replace_all(&vox, " ").to_string() }
+	pub fn pause(vox:String) -> String 
+	{
+		let output = PAUSE_RX.replace_all(&vox, " ").to_string();
+		if VERBOSE { print_if_verbose("pause", &output); }
+		output
+	}
 
 	use crate::vox_utils::PITCH_RX;
 	pub fn pitch(vox:String) -> String { 
-		PITCH_RX.replace_all(&vox, |caps: &regex::Captures| {caps[2].to_string()}).to_string()
+		let output = PITCH_RX.replace_all(&vox, |caps: &regex::Captures| {caps[2].to_string()}).to_string();
+		if VERBOSE { print_if_verbose("pitch", &output); }
+		output
 	}
 
 	use crate::vox_utils::CONTROL_CODES_RX;
-	pub fn control_codes(vox:String) -> String { CONTROL_CODES_RX.replace_all(&vox, "").to_string() }
+	pub fn control_codes(vox:String) -> String { 
+		let output = CONTROL_CODES_RX.replace_all(&vox, "").to_string();
+		if VERBOSE { print_if_verbose("control_codes", &output); }
+		output
+	}
 
 	use crate::vox_utils::CONTRACTION_RX;
 	pub fn contractions(vox:String) -> String { 
 		// No lookahead with Rust regex... ah well
-		CONTRACTION_RX.replace_all(&vox, |caps: &regex::Captures| {format!(" {} ", &caps[1])}).to_string().replace("ca n't", "can't") 
+		let output = CONTRACTION_RX.replace_all(&vox, |caps: &regex::Captures| {format!(" {} ", &caps[1])}).to_string().replace("ca n't", "can't");
+		if VERBOSE { print_if_verbose("contractions", &output); }
+		output
+	}
+
+	use crate::vox_utils::SHORTHAND_DICTIONARY;
+	pub fn remap_note_shorthand(vox:String) -> String {
+		// I don't have access to InnoDB config to decrease the min token size on Dreamhost, so just default the indexing to use the long form
+		let mut ret_val:String = vox.clone();
+		for entry in SHORTHAND_DICTIONARY.iter() {
+			ret_val = entry.0.replace_all(&ret_val, entry.1).to_string();
+		}
+		if VERBOSE { print_if_verbose("remap_note_shorthand", &ret_val); }
+		ret_val
+	}
+
+	use crate::vox_utils::TOO_SHORT_RX;
+	pub fn pad_short_words(vox:String) -> String {
+		// No lookback so keep doiing it until it's done
+		let mut prev_output : String = vox.clone();
+		let mut output : String = TOO_SHORT_RX.replace_all(&vox, |caps: &regex::Captures| {format!("{}{:_<3}{}", &caps[1], &caps[2], &caps[3])}).to_string();
+		let mut i = 0;
+		while prev_output != output {
+			prev_output = output;			
+			output = TOO_SHORT_RX.replace_all(&prev_output, |caps: &regex::Captures| {format!("{}{:_<3}{}", &caps[1], &caps[2], &caps[3])}).to_string();
+			i = i + 1;
+			if i > 100 {
+				panic!("Timeout on pad_short_words: couldn't conclude on entry {}", vox);
+			}
+		}
+		if VERBOSE { print_if_verbose("pad_short_words", &output); }
+		output
 	}
 
 	use crate::vox_utils::CLEANUP_RX;
-	pub fn cleanup(vox:String) -> String { CLEANUP_RX.replace_all(&vox, " ").to_string() }
+	pub fn cleanup(vox:String) -> String { 
+		let output = CLEANUP_RX.replace_all(&vox, " ").to_string();
+		if VERBOSE { print_if_verbose("cleanup", &output); }
+		output
+	}
 }
 
 lazy_static! { static ref VOX_DB : HashSet<String> = {
